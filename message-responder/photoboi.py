4b75aae4df9f06907f8da4f6d959760aa5666221
AP <59419397+sandpete@users.noreply.github.com>
2020-02-29 11:48:57 -0500

    fixed accessing via random #

--- message-responder/photoboi.py
+++ message-responder/photoboi.py
@@ -10,15 +10,20 @@ def photoboi():
     with open(file_path) as csvfile:
         reader = csv.reader(csvfile)
         # get row count
-        row_count = sum(1 for line in f)
+        row_count = sum(1 for line in reader)
         # picks a random number between 0 and the row_count.
         # this is how we select the image to obtain.
         rand_val = random.randint(0, row_count)
 
-        # grab the value from the random row value
-        line = next((x for i, x in enumerate(reader) if i == rand_val), None)
+    # i kept getting errors trying to read the same file, so we'll close
+    # it out and open it again
+    with open(file_path) as csvfile:
+        # turns the CSV into a list
+        data=list(csv.reader(csvfile))
+        # grab the random line value
+        rand_line = data[rand_val][0]
 
     # add the full URL
-    pic_url = "https://i.groupme.com/" + line[0]
+    pic_url = "https://i.groupme.com/" + rand_line
 
     return pic_url
